a1e76f77a6ecbe069f95db19f9ceba6896908051
MarcioPorto <mflporto@gmail.com>
2018-07-18 17:03:21 -0400

    includes new pipelines in runner file

--- pipelines.py
+++ pipelines.py
@@ -1,5 +1,9 @@
 from etl.countries import start_pipeline as start_countries_pipeline
+from etl.country_attributes import start_pipeline as start_country_attributes_pipeline
+from etl.hs_pci import start_pipeline as start_hs_pci_pipeline
 
 
 if __name__ == "__main__":
     start_countries_pipeline()
+    start_country_attributes_pipeline()
+    start_hs_pci_pipeline()
